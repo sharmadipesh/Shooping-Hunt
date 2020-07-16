db0a01771d67422a8a89f3587e8aa872bef0c9e5
sharmadipesh <dipesh.sharma@jump360.me>
2020-07-16 18:12:29 +0530

    [BUILD] Product Modal Handler

--- src/views/Dashboard/index.js
+++ src/views/Dashboard/index.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react';
-import { Card,Button } from 'antd';
+import { Card,Button,Modal,Form, Input, InputNumber } from 'antd';
 import {products} from 'dummy_data/dummy';
+import idx from 'idx'; 
+// import addToCart from "./AddtoCard";
 
 class Dashboard extends Component {
+
+    state = {
+        isAddCartOpen: false,
+        productInformation:null
+    };
+
+    addToCartHandler(isAddCartOpen,productInformation) {
+        console.log("dd")
+        this.setState({ isAddCartOpen,productInformation });
+    }
+
     render() {
         return (
             <div>
@@ -24,13 +37,66 @@ class Dashboard extends Component {
                                     <div>Qty : {product.qty}</div>
                                     <div>Price : {product.price}</div>
                                 </div>
-                                <Button type="primary"  block disabled={product.qty === 0 ? true :false}>
+                                <Button type="primary" onClick={() => this.addToCartHandler(true,product)}  block disabled={product.qty === 0 ? true :false}>
                                     Add to cart
                                 </Button>
                             </Card>
                         </div>
                     )}
                 </div>
+                <Modal
+                    title={idx(this.state.productInformation,_=>_.Name)} 
+                    centered
+                    visible={this.state.isAddCartOpen}
+                    // onOk={() => this.addToCartHandler(false)}
+                    onCancel={() => this.addToCartHandler(false)}
+                    footer={false}
+                >
+                    <div className="d-flex">
+                        <div className="mr-20">
+                            <img 
+                                alt={idx(this.state.productInformation,_=>_.Name)} 
+                                src={idx(this.state.productInformation,_=>_.image)}
+                                height="200px"
+                                width="175px"
+                            />
+                        </div>
+                        <div>
+                            <div  className="product-modal-information">
+                                Qty : {idx(this.state.productInformation,_=>_.qty)}
+                            </div>
+                            <div  className="product-modal-information mt-10">
+                                Price : {idx(this.state.productInformation,_=>_.price)}
+                            </div>
+                            <Form  
+                                name="nest-messages" 
+                                className="mt-20"
+                                // onFinish={onFinish} 
+                                // validateMessages={validateMessages}
+                            >
+    
+                                <Form.Item
+                                    name="qty"
+                                    label="Add Qty"
+                                    rules={[
+                                        {
+                                            type: 'number',
+                                            min: 0,
+                                            max: idx(this.state.productInformation,_=>_.qty),
+                                        },
+                                    ]}
+                                >
+                                    <InputNumber />
+                                </Form.Item>
+                                <Form.Item>
+                                    <Button type="primary" className="mt-30 ml-40" htmlType="submit">
+                                        Submit
+                                    </Button>
+                                </Form.Item>
+                            </Form>
+                        </div>
+                    </div>
+                </Modal>
             </div>
         );
     }
--- src/views/components/Header.js
+++ src/views/components/Header.js
@@ -3,10 +3,16 @@ import React, { Component } from 'react';
 class Header extends Component {
     render() {
         return (
-            <div className="header-style-container d-flex align-items-center">
+            <div className="header-style-container d-flex align-items-center justify-content-between">
                 <div className="d-flex align-items-center">
                         <img alt="login" src="img/shop.svg" height="30px"/>
                         <div className="company-name">ShoppingHunt</div>
+                </div>
+                <div className="shooping-cart">
+                    <img alt="shopping-cart" src="img/supermarket.svg"/>
+                    <div className="alert-container">
+                        1
+                    </div>
                 </div>
             </div>
 
