6af7d266cd58f048cabfa9a3443001215707e4b9
sharmadipesh <dipesh.sharma@jump360.me>
2020-07-16 12:18:56 +0530

    [BUILD] Dashboard Design

--- src/views/Dashboard/index.js
+++ src/views/Dashboard/index.js
@@ -1,22 +1,32 @@
 import React, { Component } from 'react';
-import { Card } from 'antd';
+import { Card,Button } from 'antd';
 import {products} from 'dummy_data/dummy';
-const { Meta } = Card;
 
 class Dashboard extends Component {
     render() {
         return (
             <div>
                 {/* Dashboard */}
-                <div className="d-flex flex-wrap p-30">
+                <div className="d-flex flex-wrap justify-content-between p-30">
                     {products.map((product,index)=>
-                        <div key={index} >
+                        <div key={index} className="mb-20 product-information">
+                            {product.qty !== 0 ? null :
+                            <img src="img/sold.svg" className="sold-out"/>}
                             <Card
                                 hoverable
                                 style={{ width: 240 }}
-                                cover={<img alt={product.name} src={product.image} />}
+                                cover={<img alt={product.Name} src={product.image} />}
                             >
-                                <Meta title="Europe Street beat" description="www.instagram.com" />
+                                <div className="mb-10 product-name">
+                                    {product.Name}
+                                </div>
+                                <div className="d-flex justify-content-between align-items-center mb-10">
+                                    <div>Qty : {product.qty}</div>
+                                    <div>Price : {product.price}</div>
+                                </div>
+                                <Button type="primary"  block disabled={product.qty === 0 ? true :false}>
+                                    Add to cart
+                                </Button>
                             </Card>
                         </div>
                     )}
@@ -27,10 +37,3 @@ class Dashboard extends Component {
 }
 
 export default Dashboard;
-{/* <Card
-    hoverable
-    style={{ width: 240 }}
-    cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
->
-    <Meta title="Europe Street beat" description="www.instagram.com" />
-</Card> */}
